388cc1e74e0aa8ee4eb598b79029d99146e6f24a
Pellegrino Testa <82523501+pellegrinotesta@users.noreply.github.com>
2025-05-02 16:56:35 +0200

    Add files via upload

---
+++ InvestAI/src/main/java/com/development/spring/invest_ai/InvestAI/InvestAiApplication.java
@@ -0,0 +1,13 @@
+package com.development.spring.invest_ai.InvestAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestAiApplication.class, args);
+	}
+
+}
---
+++ InvestAI/src/test/java/com/development/spring/invest_ai/InvestAI/InvestAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.spring.invest_ai.InvestAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
