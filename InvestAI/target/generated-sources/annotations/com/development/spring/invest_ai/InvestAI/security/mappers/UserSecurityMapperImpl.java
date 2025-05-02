20b0cade687430d81b850d4e1ba14168f75f0808
pellegrinotesta <rinotesta@live.it>
2025-05-02 17:12:44 +0200

    Aggiorna la data di generazione nei mapper utente e sicurezza

--- InvestAI/target/generated-sources/annotations/com/development/spring/invest_ai/InvestAI/security/mappers/UserSecurityMapperImpl.java
+++ InvestAI/target/generated-sources/annotations/com/development/spring/invest_ai/InvestAI/security/mappers/UserSecurityMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-02T17:10:32+0200",
+    date = "2025-05-02T17:12:24+0200",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
